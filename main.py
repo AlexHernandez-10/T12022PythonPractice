d12deaee8ba8f0544307f30a2d9e3db93517caa9
AlexHernandez-10 <830683@mystma.org>
2022-09-13 09:40:58 -0500

    3 functions

---
+++ main.py
@@ -0,0 +1,14 @@
+
+def addTwoNumbers():
+    x="Alex"
+    y="Hernandez"
+    print(x + y)
+    
+addTwoNumbers()
+
+
+
+def addFiveNumbers():
+    x="top gun"
+    y="the dark knight rises"
+    a="avengers endgame"
